47d305ec06d438611e26181d4c624390d632d22b
Andrew Bennett <andrew.bennett@unsw.edu.au>
2017-09-18 22:46:20 +1000

    card gen code

---
+++ generateCard.py
@@ -0,0 +1,98 @@
+import sys
+from cardConstants import *
+
+colors = ["RED", "BLUE", "GREEN", "YELLOW", "PURPLE"]
+colorCode = {
+        "RED":     "ff5555",
+        "BLUE":    "0000ff",
+        "GREEN":   "55aa55",
+        "YELLOW":  "ffaa00",
+        "PURPLE":  "800080"
+}
+
+
+def main():
+    makeAllImages()
+    if len(sys.argv) != 3:
+        print "Usage: python %s number color" % sys.argv[0]
+        print "eg python %s 5 RED" % sys.argv[0]
+        exit(1)
+
+    number = getNumber(sys.argv[1])
+    color = getColor(sys.argv[2])
+
+    if color is None or number is None:
+        print "invalid input"
+        exit(1)
+
+
+    overall_image = makeImage(number, color)
+
+    with open("test.svg", "w") as outfile:
+        outfile.write(overall_image)
+
+def makeAllImages():
+    for number in range(0,16):
+        for color in colors:
+            overall_image = makeImage(number, color)
+
+            cardname = "card-%s-%d.svg" % (color, number)
+            with open(cardname, "w") as outfile:
+                outfile.write(overall_image)
+
+
+
+def getColor(arg):
+    if arg in colors:
+        return arg
+    else:
+        return None
+
+def getNumber(arg):
+    try:
+        if int(arg) >= 0 and int(arg) <= 15:
+            return int(arg)
+        else:
+            return None
+    except:
+        return None
+
+
+def makeImage(number, color):
+    inner_card = makeInnerCard(number, color)
+
+    overall_image = outer_svg + outer_card + inner_card  + end_svg
+
+    return overall_image
+
+def makeInnerCard(number, color):
+    inner_card = getColorFill(color) + card_white_center + \
+            getCardCenterNum(number, color) + getCardTopLeftNum(number) + \
+            getCardBottomRightNum(number)
+
+    return inner_card
+
+def getColorFill(color):
+    return card_color_fill % colorCode[color]
+
+def getCardCenterNum(number, color):
+    print card_centers[number]
+    return card_center_outer % (colorCode[color], card_centers[number])
+
+def getCardTopLeftNum(number):
+    thing = card_topleft_outer % topleft[number]
+    if number > 9:
+        return matrix_transform_upper + card_topleft_outer % \
+    card_centers[number] + matrix_close_group
+    else:
+        return thing
+
+def getCardBottomRightNum(number):
+    if number > 9:
+        return matrix_transform_lower + card_topleft_outer % \
+        card_centers[number] + matrix_close_group
+    return card_bottomright_outer % bottomright[number]
+
+if __name__ == "__main__":
+    main()
+
