5b04d7252a8ab8a7eafd20a12459c7712d0ea249
Andrew Bennett <andrew.bennett@unsw.edu.au>
2017-09-19 16:33:59 +1000

    make code nicer

--- generateCard.py
+++ generateCard.py
@@ -1,7 +1,6 @@
 import sys
 from cardConstants import *
 
-colors = ["RED", "BLUE", "GREEN", "YELLOW", "PURPLE"]
 colorCode = {
         "RED":     "ff5555",
         "BLUE":    "0000ff",
@@ -10,18 +9,19 @@ colorCode = {
         "PURPLE":  "800080"
 }
 
-suits = {
-        "HEARTS":0,
-        "DIAMONDS":1,
-        "CLUBS":2,
-        "SPADES":3,
-        "QUESTIONS":4
-}
-
+suits = [
+    "HEARTS",
+    "DIAMONDS",
+    "CLUBS",
+    "SPADES",
+    "QUESTIONS"
+]
 
 def main():
-    makeAllImages()
-    if len(sys.argv) != 4:
+    if "IMAGES_PLS" in sys.argv:
+        makeAllImages()
+        return
+    elif len(sys.argv) != 4:
         print "Usage: python %s number color suit" % sys.argv[0]
         print "eg python %s 5 RED HEARTS" % sys.argv[0]
         exit(1)
@@ -31,12 +31,8 @@ def main():
     suit = getSuit(sys.argv[3])
 
     if color is None or number is None or suit is None:
-        print color
-        print number
-        print suit
-        print "invalid input"
+        print "invalid input: %s, %s, %s" % (color, number, suit)
         exit(1)
-
 
     overall_image = makeImage(number, color, suit)
 
@@ -44,63 +40,51 @@ def main():
         outfile.write(overall_image)
 
 def makeAllImages():
-    for number in range(0,16):
-        for color in colors:
-            for suit in suits.iterkeys():
-                overall_image = makeImage(number, color, suit)
+    for number in range(16):
+        for color in colorCode.iteritems():
+            for suit in suits:
+                overall_image = makeImage(number, color[1], suit)
 
-                cardname = "card-%s-%d-%s.svg" % (color, number, suit)
+                cardname = "card-%s-%d-%s.svg" % (color[0], number, suit)
                 with open(cardname, "w") as outfile:
                     outfile.write(overall_image)
 
-
-
 def getColor(arg):
-    if arg in colors:
-        return arg
-    else:
-        return None
+    return colorCode.get(arg)
 
 def getNumber(arg):
     try:
         if int(arg) >= 0 and int(arg) <= 15:
             return int(arg)
-    except:
+    except ValueError:
         pass
-    return None
 
 def getSuit(arg):
-    if arg in suits.iterkeys():
+    if arg in suits:
         return arg
-    else:
-        return None
-
 
 def makeImage(number, color, suit):
     inner_card = makeInnerCard(number, color, suit)
-
     overall_image = outer_svg + outer_card + inner_card  + end_svg
-
     return overall_image
 
 def makeInnerCard(number, color, suit):
-    inner_card = getColorFill(color) + card_white_center + \
-            getCardCenterNum(number, color) + \
-            getCardTopLeftNum(number) + \
-            getCardTopLeftSuit(suit) + \
-            getCardBottomRightNum(number) + \
-            getCardBottomRightSuit(suit)
-
+    inner_card = (getColorFill(color) + card_white_center +
+                  getCardCenterNum(number, color) +
+                  getCardTopLeftNum(number) +
+                  getCardTopLeftSuit(suit) +
+                  getCardBottomRightNum(number) +
+                  getCardBottomRightSuit(suit))
     return inner_card
 
 def getColorFill(color):
-    return card_color_fill % colorCode[color]
+    return card_color_fill % color
 
 def getCardCenterNum(number, color):
     card_center = card_centers[number]
     if number > 9:
         card_center = newLetters3[number-10]
-    return card_center_outer % (colorCode[color], card_center)
+    return card_center_outer % (color, card_center)
 
 def getCardTopLeftNum(number):
     thing = card_topleft_outer % topleft[number]
@@ -117,10 +101,10 @@ def getCardBottomRightNum(number):
     return card_bottomright_outer % bottomright[number]
 
 def getCardTopLeftSuit(suit):
-    return suits_top[suits[suit]]
+    return suits_top[suit]
 
 def getCardBottomRightSuit(suit):
-    return suits_bottom[suits[suit]]
+    return suits_bottom[suit]
 
 if __name__ == "__main__":
     main()
